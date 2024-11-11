7027c00083ed41abe42f5e114e8a8e0406d343de
Your Name <you@example.com>
2024-11-11 11:02:04 +0530

    first commit1

---
+++ src/sam.js
@@ -0,0 +1,90 @@
+
+/*
+let a = 5 ;
+ function sam(){
+
+
+    console.log(a);
+ }
+ function main(){
+    console.log(a);
+    sam();
+ }
+ main();
+
+ 
+function Hello(){
+
+    a = 2200109 ; b = 9 ;
+    console.log( a + "+"+ b +"="+(a+b) );
+    console.log(a+"-"+b+"="+(a-b));
+    console.log(a+"*"+b+"="+(a*b));
+    console.log(a+"/"+b+"="+(a/b));
+    console.log(a+"%"+b+"="+(a%b));
+   let c = 33;
+   console.log(c);
+   c+=3;
+   console.log(c);
+   c-=3;
+   console.log(c);
+   c/=3;
+   console.log(c);
+   c%=3;
+   console.log(c);
+
+}
+Hello();
+
+function Hello(){
+    let a= 5; b =5; c= 6;
+    if(a=b)
+        console.log("a=b");
+    else
+    console.log("ecp");
+    if(a==b)
+        console.log("a==b");
+    if(a!=b)
+        console.log("a!=b");
+    if(a!==b)
+        console.log("a!==b");
+    else
+    console.log("a==b");
+    if(a!==c)
+        console.log("a!==c");
+    if(a<c)
+        console.log("a<c");
+    if(a<=b)
+        console.log("a<=b");
+    if(a>b)
+        console.log("c>b");
+    if(a=>b)
+        console.log("a=>b");
+
+
+}
+Hello();
+*/
+let a= 5; b= 6;
+function Hello(){
+    if(a&&b){
+        console.log("a&&b");
+    }
+    else{
+        console.log("a");
+    }
+        if(a>0 && b>0){
+console.log("a and b >0");
+        }
+        if(a>0 || b>0){
+            console.log( "a or b>0");
+
+        }
+        if(!(a==b)){
+            console.log("a==b");
+        }
+        if(!(a===b)){
+            console.log("a===b");
+        }
+    }
+
+Hello();
